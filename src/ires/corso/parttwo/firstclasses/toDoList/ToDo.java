f436f693b8a5eb4235507e77e1a341554ee5e064
DeGiorgiS <stefano.degio@gmail.com>
2021-06-29 13:56:29 +0200

    Secondo commit impostazione esercizio ToDo

--- src/ires/corso/parttwo/firstclasses/toDoList/ToDo.java
+++ src/ires/corso/parttwo/firstclasses/toDoList/ToDo.java
@@ -1,15 +1,112 @@
 package ires.corso.parttwo.firstclasses.toDoList;
 
 import java.io.Serializable;
+import java.time.LocalDate;
 
 public class ToDo implements Serializable {
 
+
+    private enum Priorities {URGENTE, NORMALE, POCO_IMPORTANTE}; //todo controllare i livelli di priorità
+
+    private enum States {DA_FARE, IN_ESECUZIONE, COMPLETATA, ANNULLATA};
     // classe principale
+    private static Long countID;
+    private final Long entityID;
+    private String title;
+    private String description;
+    private final LocalDate dateOfCreation;
+    private LocalDate dateOfExpiration;
+    private Priorities priority;
+    private States state;
+
+
 
     // ...costruttore con ID incrementale...
+    public ToDo(){
+        entityID = setID();
+        title = null;
+        description = null;
+        dateOfCreation = LocalDate.now();
+        dateOfExpiration = null;
+        priority = Priorities.URGENTE;
+        state = States.DA_FARE;
+    }
 
+    public ToDo(String t, String d, LocalDate date, Priorities p, States s){
+        entityID = setID();
+        title = t;
+        description = d;
+        dateOfCreation = LocalDate.now();
+        dateOfExpiration = date;
+        priority = p;
+        state = s;
+    }
+
+    private ToDo(Long id){
+        entityID = id;
+        title = getTitle();
+        description = getDescription();
+        dateOfCreation = getDateOfCreation();
+        dateOfExpiration = getDateOfExpiration();
+        priority = getPriority();
+        state = getState();
+    }
+
+    //metodo interno per prendere il minor ID libero e poi incrementare.
+    //migliorabile con un controllo sul minor ID non utilizzato (es. ID 2 libero, ma ID dal 3 al 6 usati)
+    private Long setID(){
+        return countID++;
+    }
+
+    public static Long getCountID() {
+        return countID;
+    }
+
+    public Long getEntityID() {
+        return entityID;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public LocalDate getDateOfCreation() {
+        return dateOfCreation;
+    }
+
+    public LocalDate getDateOfExpiration() {
+        return dateOfExpiration;
+    }
+
+    public Priorities getPriority() {
+        return priority;
+    }
+
+    public States getState() {
+        return state;
+    }
+
+    // fabbrica una copia esatta del To-Do (compreso l'ID)
+    // pensavo di creare un costruttore "volante" qui dentro che mi permettesse di riassegnare l'ID
     public ToDo cloneForUpdate() {
-        // fabbrica una copia esatta del To-Do (compreso l'ID)
-        return null; //todo da cambiare il return null
+
+        /*@Override
+        public ToDo(){
+            entityID = getEntityID();
+            title = getTitle();
+            description = getDescription();
+            dateOfCreation = getDateOfCreation();
+            dateOfExpiration = getDateOfExpiration();
+            priority = getPriority();
+            state = getState();
+        }
+         */
+        Long id = getEntityID();
+        ToDo copiedToDo = new ToDo(id);
+        return copiedToDo;
     }
 }
--- src/ires/corso/parttwo/firstclasses/toDoList/ToDoRepository.java
+++ src/ires/corso/parttwo/firstclasses/toDoList/ToDoRepository.java
@@ -1,9 +1,7 @@
 package ires.corso.parttwo.firstclasses.toDoList;
 
 import java.io.Serializable;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class ToDoRepository implements Serializable {
 
@@ -32,23 +30,43 @@ public class ToDoRepository implements Serializable {
     Map<Long, ToDo> _data = new HashMap<>();
 
     public void delete(Long ID) {
-
+        //todo print "stai eliminando l'oggetto -descrizione-, sei sicuro?", se SI...
+        _data.remove(ID);
     };
 
     public void add(ToDo t) {
         // si deve entrare nell'oggetto t e leggere il suo ID
         // per poi salvarlo nella mappa correttamente (con put(ID, t))
+        //todo print "stai aggiungendo l'oggetto -descrizione-, sei sicuro?", se SI...
+        _data.put(t.getEntityID(), t);
     }
 
     public void update(ToDo t) {
         // si prende l'ID dall'oggetto t
         // si recupera dalla mappa il TO-DO corrispondente con get(t), per controllo
         // si sostituisce con put(ID, t)
+        Long id = t.getEntityID();
+        _data.get(id);
+        //todo print "sei sicuro di voler eliminare l'oggetto... con descrizione dell'oggetto?", se SI...
+        _data.remove(id);
+        _data.put(id, t);
     }
 
+    //todo controllo se il ciclo While funziona come dovrebbe
     public List<ToDo> getToDoList() {
-        // restituisce lista di tutti i TO-DO esistenti
-        return null;
+        List<ToDo> toDoAsList= new ArrayList<>();
+
+        //con metodo compatto
+        _data.values().addAll(toDoAsList);
+
+        //con iterazione
+        /*
+        Iterator<ToDo> iToDo = _data.values().iterator();
+        while(iToDo.hasNext()){
+            toDoAsList.add(iToDo.next());
+        }
+         */
+        return toDoAsList;
     }
 
     public void writeToFile(String fileName) {
